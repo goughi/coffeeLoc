2fe0c34c999edb77cf3bada23ea841f98d8ae11a
goughi <goughi@hotmail.com>
2015-12-04 11:36:00 +0000

    v1.6

--- coffee/Controllers/ReviewController.cs
+++ coffee/Controllers/ReviewController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Coffee.Models;
+using PagedList;
 
 namespace coffee.Controllers
 {
@@ -15,10 +16,69 @@ namespace coffee.Controllers
         private DrinkContext db = new DrinkContext();
 
         // GET: Review
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
         {
             var reviews = db.Reviews.Include(r => r.CoffeeStore);
-            return View(reviews.ToList());
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Custname_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewBag.RatingSortParm = sortOrder == "Rating" ? "rating_desc" : "Rating";
+            ViewBag.LocationSortParm = sortOrder == "Location" ? "location_desc" : "Location";
+            ViewBag.StoreSortParm = sortOrder == "Store" ? "store_desc" : "Store";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                reviews = reviews.Where(r=> r.CustomerName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    reviews = reviews.OrderByDescending(r => r.CustomerName);
+                    break;
+                case "Date":
+                  reviews = reviews.OrderBy(r => r.ReviewDate);
+                    break;
+                case "date_desc":
+                    reviews = reviews.OrderByDescending(r => r.ReviewDate);
+                    break;
+                case "Rating":
+                    reviews = reviews.OrderBy(r => r.Rating);
+                    break;
+                case "rating_desc":
+                    reviews = reviews.OrderByDescending(r => r.Rating);
+                    break;
+                case "Location":
+                    reviews = reviews.OrderBy(r => r.CoffeeStore.Location);
+                    break;
+                case "location_desc":
+                    reviews = reviews.OrderByDescending(r => r.CoffeeStore.Location);
+                    break;
+                case "Store":
+                    reviews = reviews.OrderBy(r => r.CoffeeStore.StoreName);
+                    break;
+                case "store_desc":
+                    reviews = reviews.OrderByDescending(r => r.CoffeeStore.StoreName);
+                    break;
+                default:
+                    reviews = reviews.OrderBy(r => r.CustomerName);
+                    break;
+            }
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(reviews.ToPagedList(pageNumber, pageSize));
+
         }
 
         // GET: Review/Details/5
@@ -41,6 +101,7 @@ namespace coffee.Controllers
         {
             Review review = new Review() { Rating = 4, ReviewDate = DateTime.Now };
             ViewBag.Eircode = new SelectList(db.CoffeeStores, "Eircode", "StoreName");
+            
             return View(review);
         }
 
